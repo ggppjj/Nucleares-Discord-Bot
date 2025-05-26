d93ed018b96c0d8914420db37105d2c7c17d9a18
Gabriel Jones <1223390+ggppjj@users.noreply.github.com>
2025-05-26 18:26:14 -0400

    Update LibNuclearesWeb to use correct default location and port, add config file appConfig.json and commandline args for IpAddress and Port. Commandline takes precedence. Cleanup.

--- Program.cs
+++ Program.cs
@@ -5,22 +5,41 @@ using LibNuclearesWeb.NuclearesWeb;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
-_ = args;
+CancellationTokenSource? shutdown = new();
+DiscordSocketConfig? config = new() { LogLevel = LogSeverity.Info };
+DiscordSocketClient? client = new(config);
 
-var shutdown = new CancellationTokenSource();
-var nuclearesWebController = new NuclearesWeb();
+var appConfig = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appConfig.json", optional: true, reloadOnChange: true)
+    .Build();
+
+var configIp = appConfig["IpAddress"];
+var configPort = appConfig["Port"];
+
+var gameIp = configIp;
+int? gamePort = null;
+if (int.TryParse(configPort, out var configPortParsed))
+    gamePort = configPortParsed;
+
+var ipArg = args.FirstOrDefault(e => e.StartsWith("/IpAddress="));
+if (ipArg is not null)
+    gameIp = ipArg["/IpAddress=".Length..];
+
+var argPort = args.FirstOrDefault(e => e.StartsWith("/Port="));
+if (argPort is not null && int.TryParse(argPort["/Port=".Length..], out var argPortParsed))
+    gamePort = argPortParsed;
+
+NuclearesWeb? nuclearesWebController = new(gameIp, gamePort);
 
 Console.CancelKeyPress += (_, e) =>
 {
     e.Cancel = true;
     shutdown.Cancel();
 };
-
 AppDomain.CurrentDomain.ProcessExit += (_, _) => shutdown.Cancel();
 
-var config = new DiscordSocketConfig { LogLevel = LogSeverity.Info };
-var client = new DiscordSocketClient(config);
-client.Ready += ClientReady;
+client.Ready += () => OnClientReady(client);
 client.SlashCommandExecuted += SlashCommandHandler;
 client.Log += (logEntry) =>
 {
@@ -32,21 +51,37 @@ client.Log += (logEntry) =>
     return Task.CompletedTask;
 };
 
-var appConfig = new ConfigurationBuilder()
+var tokenConfig = new ConfigurationBuilder()
     .SetBasePath(AppContext.BaseDirectory)
     .AddJsonFile("token.json", optional: false, reloadOnChange: true)
     .Build();
 
-var token = appConfig["Token"];
+var token = tokenConfig["Token"];
+if (string.IsNullOrWhiteSpace(token))
+{
+    Console.WriteLine("Missing bot token in token.json");
+    return;
+}
 
 await client.LoginAsync(TokenType.Bot, token);
 await client.StartAsync();
 
-async Task ClientReady()
+try
 {
-    List<ApplicationCommandProperties> applicationCommandProperties = [];
+    await Task.Delay(Timeout.Infinite, shutdown.Token);
+}
+catch (TaskCanceledException)
+{
+    await client.LogoutAsync();
+    await client.StopAsync();
+    client.Dispose();
+}
+return;
 
-    applicationCommandProperties.Add(
+async Task OnClientReady(DiscordSocketClient readyClient)
+{
+    List<ApplicationCommandProperties> applicationCommandProperties =
+    [
         new SlashCommandBuilder()
             .WithName("execute-command")
             .WithDescription("Execute a command on the running user's game!")
@@ -56,12 +91,12 @@ async Task ClientReady()
                 "Command to run.",
                 isRequired: true
             )
-            .Build()
-    );
+            .Build(),
+    ];
 
     try
     {
-        _ = await client.BulkOverwriteGlobalApplicationCommandsAsync(
+        _ = await readyClient.BulkOverwriteGlobalApplicationCommandsAsync(
             [.. applicationCommandProperties]
         );
     }
@@ -85,15 +120,4 @@ async Task SlashCommandHandler(SocketSlashCommand command)
             await command.RespondAsync("Command unknown, somehow!");
             break;
     }
-}
-
-try
-{
-    await Task.Delay(Timeout.Infinite, shutdown.Token);
-}
-catch (TaskCanceledException)
-{
-    await client.LogoutAsync();
-    await client.StopAsync();
-    client.Dispose();
 }
